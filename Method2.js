05df1bb10881a9f7f1847b0dcf26044982eb45f7
Fernando Vargas Mendez <fernanvargas.12@gmail.com>
2022-09-07 14:00:23 -0300

    Update Method2.js

--- Method2.js
+++ Method2.js
@@ -2,3 +2,13 @@ document.addEventListener("visibilitychange", () => {
     // it could be either hidden or visible
     document.title = document.visibilityState;    
 });
+
+
+//You can use these to add many functionality to your website such as stop music or video when the user loses focus
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === 'visible') {
+    music.play();
+  } else {
+    music.pause();
+  }
+});
